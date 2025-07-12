eba67edd8ba2718ec280c1b1a7a07e3713b5b762
Akira HIGUCHI <akira_higuchi@tailor.tech>
2025-07-12 21:06:10 +0900

    improve comment

--- src/extension.ts
+++ src/extension.ts
@@ -379,7 +379,7 @@ class JsonlPreviewPanel {
               return [multilineMarker, ...lines];
             }
             return value;
-          }, 2).replace(new RegExp(`\\[\\n\\s+"\\${multilineMarker}",\\n`, 'g'), `[ // Transformed for preview multiline string\n`);
+          }, 2).replace(new RegExp(`"\\${multilineMarker}",`, 'g'), `// Displayed as array for multiline string visibility`);
         } catch (e) {
           jsonContent = 'Invalid JSON on line ' + lineNumber;
         }
